37ae869771c67368588bc1ffb844c94a8f486655
Sudhanshu Srivastava <48932266+sudsho@users.noreply.github.com>
2020-05-25 12:55:48 +0530

    refactor: api.parse_resume_text takes use_spacy flag for tests

--- src/api.py
+++ src/api.py
@@ -1,36 +1,39 @@
 """End-to-end parse: text -> structured fields."""
 from . import extract
 from . import parse
-from . import skills
+from . import skills as skills_mod
 from . import email_phone
-from . import sections
+from . import sections as sec_mod
 
 
-def parse_resume_text(text):
-    ents = parse.parse_entities(text)
-    name = parse.guess_name(text, ents=ents)
-    emails = email_phone.find_emails(text)
-    phones = email_phone.find_phones(text)
-    urls = email_phone.find_urls(text)
-    secs = sections.split_sections(text)
+def parse_resume_text(text, use_spacy=True):
+    """Parse pre-extracted text. Set use_spacy=False to skip NER (faster, lighter)."""
+    secs = sec_mod.split_sections(text)
     skills_text = secs.get("skills", text)
-    sk = skills.find_skills(skills_text)
-    edu = parse.find_education(secs.get("education", text))
-    return {
-        "name": name,
-        "emails": emails,
-        "phones": phones,
-        "urls": urls,
-        "education": edu,
-        "skills": sk,
-        "organizations": ents.get("ORG", []),
-        "dates": ents.get("DATE", []),
+
+    out = {
+        "name": None,
+        "emails": email_phone.find_emails(text),
+        "phones": email_phone.find_phones(text),
+        "urls": email_phone.find_urls(text),
+        "education": parse.find_education(secs.get("education", text)),
+        "skills": skills_mod.find_skills(skills_text),
+        "organizations": [],
+        "dates": [],
         "sections": list(secs.keys()),
     }
 
+    if use_spacy:
+        ents = parse.parse_entities(text)
+        out["name"] = parse.guess_name(text, ents=ents)
+        out["organizations"] = ents.get("ORG", [])
+        out["dates"] = ents.get("DATE", [])
 
-def parse_resume_file(path):
+    return out
+
+
+def parse_resume_file(path, use_spacy=True):
     text = extract.extract_text(path)
-    out = parse_resume_text(text)
+    out = parse_resume_text(text, use_spacy=use_spacy)
     out["_text_length"] = len(text)
     return out
