1fab7d189bf91269c7904d0222473fc0ed942b93
Sudhanshu Srivastava <48932266+sudsho@users.noreply.github.com>
2020-05-26 12:07:54 +0530

    fix flaky test - pytest.ini + conftest path setup

---
+++ conftest.py
@@ -0,0 +1,7 @@
+"""Make src importable in tests without installing the package."""
+import os
+import sys
+
+ROOT = os.path.dirname(os.path.abspath(__file__))
+if ROOT not in sys.path:
+    sys.path.insert(0, ROOT)
